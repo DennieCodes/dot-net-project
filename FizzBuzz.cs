c53c464d4f8ebc245f55deddb66809ee9db7ef37
Dennie <oculareo@gmail.com>
2023-12-30 11:40:07 -0500

    MS Learn basics

---
+++ FizzBuzz.cs
@@ -0,0 +1,16 @@
+// for (int count=0; count <=100; count++)
+// {
+//     if (count % 3 == 0 && count % 5 == 0)
+//     {
+//         Console.WriteLine(count + " FizzBuzz");
+//     } else if (count % 3 == 0)
+//     {
+//         Console.WriteLine(count + " Fizz");
+//     } else if (count % 5 == 0)
+//     {
+//         Console.WriteLine(count + " Buzz");
+//     } else
+//     {
+//         Console.WriteLine(count);
+//     }
+// }
