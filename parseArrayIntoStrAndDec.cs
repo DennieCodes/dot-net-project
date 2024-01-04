93235c1693308ec91dd026d271e89bdead60c1db
Dennie <oculareo@gmail.com>
2024-01-04 15:49:52 -0500

    Completed Module 3, Part 4 MS Learn path

---
+++ parseArrayIntoStrAndDec.cs
@@ -0,0 +1,19 @@
+// string[] values = { "12.3", "45", "ABC", "11", "DEF" };
+// string message = "";
+// float total = 0;
+// float decimalNumber;
+
+// foreach (string item in values)
+// {
+//   if (float.TryParse(item, out decimalNumber))
+//   {
+//     total += decimalNumber;
+//   }
+//   else
+//   {
+//     message += item;
+//   }
+// }
+
+// Console.WriteLine($"Message: {message}");
+// Console.WriteLine($"Total: {total}");
